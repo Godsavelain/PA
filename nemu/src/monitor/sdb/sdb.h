39af203af4211f9f56ed04fea78174a633fdadb8
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-24 09:09:30 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:09:30 up 3 days, 17:00,  6 users,  load average: 0.35, 0.26, 0.18

--- nemu/src/monitor/sdb/sdb.h
+++ nemu/src/monitor/sdb/sdb.h
@@ -3,13 +3,13 @@
 
 #include <common.h>
 
-// typedef struct watchpoint {
-//   int NO;
-//   bool is_empty;
-//   struct watchpoint *next;
-//   char expr[50];
-//   /* TODO: Add more members if necessary */
-// } WP;
+typedef struct watchpoint {
+  int NO;
+  bool is_empty;
+  struct watchpoint *next;
+  char expr[50];
+  /* TODO: Add more members if necessary */
+} WP;
 
 word_t expr(char *e, bool *success);
 
