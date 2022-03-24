bf5aca1a97238e6bf9e285c2f53169b56124694e
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-24 02:03:28 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:03:28 up 3 days,  9:54,  7 users,  load average: 0.06, 0.12, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 
 enum {
-  TK_NOTYPE = 256, TK_EQ, TK_NUMBER ,TK_HEX_NUMBER
+  TK_NOTYPE = 256, TK_EQ, TK_NUMBER ,TK_HEX_NUMBER ,TK_REG ,TK_NEQ ,TK_AND 
 
   /* TODO: Add more token types */
 
@@ -33,11 +33,11 @@ static struct rule {
   {"\\)", ')'},         // right parentheses
   {"0|([1-9][0-9]*)",TK_NUMBER },         // number
   {"0x0|(0x([1-9]|[a-f]|[A-F])([0-9]|[a-f]|[A-F])*)",TK_HEX_NUMBER },         // hex number
-  {"\\)", ')'},         // right parentheses
-  {"\\)", ')'},         // right parentheses
-  {"\\)", ')'},         // right parentheses
-  {"\\)", ')'},         // right parentheses
-  
+  {"\\$0|\\$ra|\\$sp|\\$gp|\\$tp|\\$t0|\\$t1|\\$t2|\\s0|\\$s1|\\$a0|\
+    \\$a1|\\$a2|\\$a3|\\$a4|\\$a5|\\a6|\\$a7|\\$s2|\\$s3|\\$s4|\\$s5|\
+    \\$s6|\\$s7|\\s8|\\$s9|\\$s10|\\$s11|\\$t3|\\$t4|\\$t5|\\$t6",TK_REG},//register
+  {"!=", TK_NEQ},         // not equal
+  {"&&", TK_AND},         // and
 };
 
 #define NR_REGEX ARRLEN(rules)
@@ -66,7 +66,7 @@ typedef struct token {
   char str[30];
 } Token;
 
-static Token tokens[70000] __attribute__((used)) = {};
+static Token tokens[100] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
@@ -108,7 +108,6 @@ static bool make_token(char *e) {
         // switch (rules[i].token_type) {
         //   default: TODO();
         // }
-
         break;
       }
     }
@@ -122,12 +121,101 @@ static bool make_token(char *e) {
   return true;
 }
 
+bool is_depointer(int search_point)//to judge if a '*' is a depointer
+{
+  bool is_pointer;
+  if(search_point == 0)
+  { 
+    is_pointer = true;
+  }
+  search_point--;
+
+  while(tokens[search_point].type == TK_NOTYPE )
+  {
+    if(search_point == 0)
+    {
+      is_pointer = true;
+      return is_pointer;
+      break;
+    }
+    search_point--;
+  }
+
+  if((tokens[search_point].type == TK_HEX_NUMBER) || (tokens[search_point].type == TK_NUMBER) || (tokens[search_point].type == ')'))
+  {
+    is_pointer = false;
+  }
+  else{
+    is_pointer = true;
+  }
+  
+  return is_pointer;
+}
+
 int get_inferior(int begin ,int end){
   //printf("begin:%d end:%d \n",begin,end);
   int temp_end = end;
   int want_left_para = 0;
   while(temp_end > begin){
     //printf("temp_end:%d want_left:%d \n",temp_end,want_left_para);
+    //judge &&
+  if(want_left_para > 0){
+      if(tokens[temp_end].type == '('){
+        want_left_para--;
+      }
+      if(tokens[temp_end].type == ')'){
+        want_left_para++;
+      }
+      temp_end--;
+      continue;
+
+    }
+    switch(tokens[temp_end].type){
+      case ')':
+        want_left_para++;
+        temp_end--;
+        continue;
+      case TK_AND:
+        return temp_end;
+      default:
+        temp_end--;
+        continue;
+    }
+  }
+  // jude == or !=
+  want_left_para = 0;
+  temp_end = end;
+  while(temp_end > begin){
+  if(want_left_para > 0){
+      if(tokens[temp_end].type == '('){
+        want_left_para--;
+      }
+      if(tokens[temp_end].type == ')'){
+        want_left_para++;
+      }
+      temp_end--;
+      continue;
+
+    }
+    switch(tokens[temp_end].type){
+      case ')':
+        want_left_para++;
+        temp_end--;
+        continue;
+      case TK_EQ:
+        return temp_end;
+      case TK_NEQ:
+        return temp_end;
+      default:
+        temp_end--;
+        continue;
+    }
+  }
+    want_left_para = 0;
+    temp_end = end;
+    //+ -
+    while(temp_end > begin)
+    {
     if(want_left_para > 0){
       if(tokens[temp_end].type == '('){
         want_left_para--;
@@ -156,7 +244,7 @@ int get_inferior(int begin ,int end){
         continue;
     }
   }
-    //assert(want_left_para == 0);
+    //* /
     want_left_para = 0;
     temp_end = end;
     while(temp_end > begin)
@@ -181,6 +269,15 @@ int get_inferior(int begin ,int end){
         temp_end--;
         continue;
       case '*':
+        if(is_depointer(temp_end))
+        {
+          temp_end--;
+          continue;
+        }
+        else
+        {
+        return temp_end;
+        }
       case '/':
         return temp_end;
       default:
@@ -188,10 +285,41 @@ int get_inferior(int begin ,int end){
         continue;
     } 
   }
-  //printf("token end type %c want_left %d\n",tokens[end].type,want_left_para);
-    
-    assert(0); 
-    
+
+  want_left_para = 0;
+  temp_end = end;
+  while(temp_end > begin){
+  if(want_left_para > 0){
+      if(tokens[temp_end].type == '('){
+        want_left_para--;
+      }
+      if(tokens[temp_end].type == ')'){
+        want_left_para++;
+      }
+      temp_end--;
+      continue;
+
+    }
+    switch(tokens[temp_end].type){
+      case ')':
+        want_left_para++;
+        temp_end--;
+        continue;
+      case '*':
+      if(is_depointer(temp_end))
+        {
+          return temp_end;
+        }
+        temp_end--;
+        continue;
+        
+      default:
+        temp_end--;
+        continue;
+    }
+  }
+  //printf("token end type %c want_left %d\n",tokens[end].type,want_left_para);    
+    assert(0);  
 }
 
 uint eval(int p, int q ,bool* success)
