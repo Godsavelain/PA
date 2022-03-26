627b6aefb0d03123438e928f719d2d27c419c1fe
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-26 05:16:23 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:16:23 up 5 days, 13:07,  6 users,  load average: 0.22, 0.08, 0.05

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -8,7 +8,7 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_U, TYPE_S,TYPE_R, TYPE_B, TYPE_J,
   TYPE_N, // none
 };
 
@@ -22,6 +22,8 @@ enum {
 static word_t immI(uint32_t i) { return SEXT(BITS(i, 31, 20), 12); }
 static word_t immU(uint32_t i) { return SEXT(BITS(i, 31, 12), 20) << 12; }
 static word_t immS(uint32_t i) { return (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); }
+static word_t immB(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 12) | BITS(i, 7, 7) <<  11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1 | 0; }
+static word_t immJ(uint32_t i) { return (SEXT(BITS(i, 31, 31), 1) << 20) | BITS(i, 19, 12) <<  12 | BITS(i, 20, 20) << 11 | BITS(i, 30, 21) << 1 | 0; }
 
 static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
@@ -30,9 +32,12 @@ static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, 
   int rs2 = BITS(i, 24, 20);
   destR(rd);
   switch (type) {
-    case TYPE_I: src1R(rs1);     src2I(immI(i)); break;
+    case TYPE_I: src1R(rs1); destR(rd); src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
+    case TYPE_R: destR(rd); src1R(rs1); src2R(rs2); break;
+    case TYPE_B: destI(immB(i)); src1R(rs1); src2R(rs2); break;
+    case TYPE_J: src1I(immJ(i)); destR(rd); break;
   }
 }
 
@@ -50,6 +55,9 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(dest) = src1 + s->pc);
   INSTPAT("??????? ????? ????? 011 ????? 00000 11", ld     , I, R(dest) = Mr(src1 + src2, 8));
   INSTPAT("??????? ????? ????? 011 ????? 01000 11", sd     , S, Mw(src1 + dest, 8, src2));
+  INSTPAT("????????????  ????? 000 ????? 00100 11", addi   , I, R(dest) = src1 + src2);
+  INSTPAT("?????????????????????????     11011 11", jal    , J, s->dnpc = s->pc + src1; R(dest) = s->snpc);
+  INSTPAT("????????????  ????? 000 ????? 11001 11", jalr   , I, s->dnpc = (src2 + src1) & (word_t)(-1); R(dest) = s->snpc);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
