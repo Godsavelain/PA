63ce7f9bfbff5bf396bd53899ef7444577f51122
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 05:32:56 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:32:56 up 8 days, 13:24, 12 users,  load average: 1.64, 1.43, 1.20

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -13,8 +13,80 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
 
-int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+int sprintf(char *out, const char *fmt, ...) 
+{
+  va_list ap;
+  char *s;
+  int d;
+
+  va_start(ap,fmt);
+
+  char origin_str[100];
+  int i = 0;
+  while(*(fmt+i) != '\0')
+  {
+    origin_str[i] = *(fmt+i);
+    i++;
+  }
+  origin_str[i] = '\0';
+  i = 0;
+  int num = 0;
+  while(1)
+  {
+    if(origin_str[i] == '\0')
+    {
+      va_end(ap);
+      *out = '\0';
+      return num;
+    }
+    if(origin_str[i] != '%') 
+    {
+      *out = origin_str[i];
+      out++;
+      i++;
+      continue;
+    }
+    else
+    {
+      i++;
+      if(origin_str[i] == 'd')
+      {
+        d = va_arg(ap, int);
+        num++;
+        i++;
+        char inverted_num[20];
+        int len = 0;
+        while(d != 0)
+        {
+          int res = d % 10;
+          inverted_num[len] = (char)(res + 48);
+          len++;
+          d = (d - res)/10;
+        }
+        while(len > 0)
+        {
+          *out = inverted_num[len-1];
+          out++;
+          len--;
+        }
+      }
+      if(origin_str[i] == 's')
+      {
+        s = va_arg(ap, char *);
+        num++;
+        i++;
+        while(*s != '\0')
+        {
+          *out = *s;
+          out++;
+          s++;
+        }
+      }
+
+    }
+    
+  }
+
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
