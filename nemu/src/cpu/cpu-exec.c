8ab78e4d740fe654b5f222bc9a1f4e35299c041d
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-29 21:15:05 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:15:05 up 9 days,  5:06, 12 users,  load average: 1.12, 1.09, 1.05

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -56,7 +56,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
     {
       if(current->is_empty == false)
       {
-        printf("%s  %u\n",current->instr_asm,current->inst);
+        printf("%s  %8u\n",current->instr_asm,current->inst);
         //printf("bbb\n");
       }
       current = current->next;
@@ -91,6 +91,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
   #ifdef CONFIG_LOOP_ITRACE
     current->inst = s->isa.inst.val;
     current->is_empty = false;
+    printf("p:%s\n",p);
     strcpy(current->instr_asm,p);
     current = current->next;
     //printf("aaa\n");
