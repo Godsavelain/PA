0e2d8686a399ed42a722e3b2636abef0ea93d830
tracer-ysyx2204 <tracer@ysyx.org>
2022-03-31 00:40:14 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:40:14 up 41 min,  3 users,  load average: 0.29, 0.40, 0.42

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -1,5 +1,6 @@
 #include <isa.h>
 #include <memory/paddr.h>
+#include <elf.h>
 
 void init_rand();
 void init_log(const char *log_file);
@@ -93,7 +94,7 @@ static void parse_elf()
 {
   if(img_file != NULL)
   {
-    printf("img file name : %s\n",img_file);
+    //printf("img file name : %s\n",img_file);
     int len = strlen(img_file);
     char elf_file[100];
     strcpy(elf_file,img_file);
@@ -102,7 +103,59 @@ static void parse_elf()
     elf_file[len-3] = 'e';
     FILE *fp = fopen(elf_file, "rb");
     Assert(fp, "Can not open '%s'", elf_file);
-    printf("elf file name : %s\n",elf_file);
+    //printf("elf file name : %s\n",elf_file);
+    Elf64_Ehdr elf_head;
+    // read elf head
+    int a;
+	  a = fread(&elf_head, sizeof(Elf64_Ehdr), 1, fp);   //fread参数1：读取内容存储地址，参数2：读取内容大小，参数3：读取次数，参数4：文件读取引擎
+	  if (a == 0) assert(0);
+	  // judge magic number
+	  if (elf_head.e_ident[0] != 0x7F ||
+		  elf_head.e_ident[1] != 'E' ||
+		  elf_head.e_ident[2] != 'L' ||
+		  elf_head.e_ident[3] != 'F')
+	  {
+		  printf("Not a ELF file\n");
+		  assert(0);
+	  } 
+    Elf64_Shdr *shdr = (Elf64_Shdr*)malloc(sizeof(Elf64_Shdr) * elf_head.e_shnum);
+    assert(shdr != NULL);
+    fseek(fp, elf_head.e_shoff, SEEK_SET);
+    a = fread(shdr, sizeof(Elf64_Shdr) * elf_head.e_shnum, 1, fp);
+    assert( a!=0 );
+    // 重置指针位置到文件流开头
+	  rewind(fp);
+	// 将fp指针移到 字符串表偏移位置处
+	  fseek(fp, shdr[elf_head.e_shstrndx].sh_offset, SEEK_SET);
+	// 第e_shstrndx项是字符串表 定义 字节 长度 char类型 数组
+	  char shstrtab[shdr[elf_head.e_shstrndx].sh_size];
+	  char *temp = shstrtab;
+    for (int i = 0; i < elf_head.e_shnum; i++)
+	{
+		if(shdr[i].sh_type != SHT_SYMTAB)
+    {
+      continue;
+    }
+    temp = shstrtab;
+		temp = temp + shdr[i].sh_name;
+    if (strcmp(temp, ".dynsym") != 0) continue;//该section名称
+		printf("节的名称: %s\n", temp);
+		printf("节首的偏移: %x\n", (uint)shdr[i].sh_offset);
+		printf("节的大小: %x\n", (uint)shdr[i].sh_size);
+    uint8_t *sign_data=(uint8_t*)malloc(sizeof(uint8_t)*shdr[i].sh_size);
+		// 依据此段在文件中的偏移读取出
+		fseek(fp, shdr[i].sh_offset, SEEK_SET);
+		a = fread(sign_data, sizeof(uint8_t)*shdr[i].sh_size, 1, fp);
+    assert( a!=0 );
+		// 显示读取的内容
+		uint8_t *p = sign_data;
+		int j = 0;
+		for (j=0; j<shdr[i].sh_size; j++)
+		{
+		    printf("%x", *p);
+            p++;
+		}
+	 }
   }
 }
 
