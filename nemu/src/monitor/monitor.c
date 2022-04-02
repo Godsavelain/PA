54e2c5e26959aebe7b96b477c90eb0c45fe0e9d9
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 07:47:32 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:47:32 up 1 day, 21:10,  3 users,  load average: 0.46, 0.46, 0.34

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -32,16 +32,6 @@ static char *diff_so_file = NULL;
 static char *img_file = NULL;
 static int difftest_port = 1234;
 
-#define NR_FUNC 50
-
-typedef struct func_range {
-  char name[30];
-  uint64_t addr;
-} FUNC;
-
-static FUNC func_pool[NR_FUNC] = {};
-static int func_num = 0;
-
 void ftrace_call(word_t pc, word_t addr)
 {
   #ifdef CONFIG_FTRACE
@@ -95,70 +85,19 @@ void ftrace_ret(word_t pc)
    return;
 }
 
-static long load_img() {
-  if (img_file == NULL) {
-    Log("No image is given. Use the default build-in image.");
-    return 4096; // built-in image size
-  }
-  //printf("img file name : %s\n",img_file);
-  FILE *fp = fopen(img_file, "rb");
-  Assert(fp, "Can not open '%s'", img_file);
+#ifdef CONFIG_FTRACE
+#define NR_FUNC 50
 
-  fseek(fp, 0, SEEK_END);
-  long size = ftell(fp);
+typedef struct func_range {
+  char name[30];
+  uint64_t addr;
+} FUNC;
 
-  Log("The image is %s, size = %ld", img_file, size);
-
-  fseek(fp, 0, SEEK_SET);
-  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
-  assert(ret == 1);
-
-  fclose(fp);
-  return size;
-}
-
-static int parse_args(int argc, char *argv[]) {
-  // printf("argnum:%d\n",argc);
-  // printf("args:%s\n",argv[0]);
-  // printf("args:%s\n",argv[1]);
-  // printf("args:%s\n",argv[2]);
-  // printf("args:%s\n",argv[3]);
-  const struct option table[] = {
-    {"batch"    , no_argument      , NULL, 'b'},
-    {"log"      , required_argument, NULL, 'l'},
-    {"diff"     , required_argument, NULL, 'd'},
-    {"port"     , required_argument, NULL, 'p'},
-    {"help"     , no_argument      , NULL, 'h'},
-    {0          , 0                , NULL,  0 },
-  };
-  int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
-    switch (o) {
-      case 'b': sdb_set_batch_mode(); break;
-      case 'p': sscanf(optarg, "%d", &difftest_port); break;
-      case 'l': log_file = optarg; break;
-      case 'd': diff_so_file = optarg; break;
-      case 1: img_file = optarg; return optind - 1;
-      default:
-        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
-        printf("\t-b,--batch              run with batch mode\n");
-        printf("\t-l,--log=FILE           output log to FILE\n");
-        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
-        printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
-        printf("\n");
-        exit(0);
-    }
-  }
-
-  return 0;
-}
+static FUNC func_pool[NR_FUNC] = {};
+static int func_num = 0;
 
 static void parse_elf() 
 {
-  #ifdef CONFIG_FTRACE
-    return;
-  #endif
-  
   if(img_file != NULL)
   {
     //printf("img file name : %s\n",img_file);
@@ -260,6 +199,66 @@ static void parse_elf()
   free(shdr);
 }
 }
+#endif
+
+static long load_img() {
+  if (img_file == NULL) {
+    Log("No image is given. Use the default build-in image.");
+    return 4096; // built-in image size
+  }
+  //printf("img file name : %s\n",img_file);
+  FILE *fp = fopen(img_file, "rb");
+  Assert(fp, "Can not open '%s'", img_file);
+
+  fseek(fp, 0, SEEK_END);
+  long size = ftell(fp);
+
+  Log("The image is %s, size = %ld", img_file, size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(guest_to_host(RESET_VECTOR), size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+  return size;
+}
+
+static int parse_args(int argc, char *argv[]) {
+  // printf("argnum:%d\n",argc);
+  // printf("args:%s\n",argv[0]);
+  // printf("args:%s\n",argv[1]);
+  // printf("args:%s\n",argv[2]);
+  // printf("args:%s\n",argv[3]);
+  const struct option table[] = {
+    {"batch"    , no_argument      , NULL, 'b'},
+    {"log"      , required_argument, NULL, 'l'},
+    {"diff"     , required_argument, NULL, 'd'},
+    {"port"     , required_argument, NULL, 'p'},
+    {"help"     , no_argument      , NULL, 'h'},
+    {0          , 0                , NULL,  0 },
+  };
+  int o;
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
+    switch (o) {
+      case 'b': sdb_set_batch_mode(); break;
+      case 'p': sscanf(optarg, "%d", &difftest_port); break;
+      case 'l': log_file = optarg; break;
+      case 'd': diff_so_file = optarg; break;
+      case 1: img_file = optarg; return optind - 1;
+      default:
+        printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
+        printf("\t-b,--batch              run with batch mode\n");
+        printf("\t-l,--log=FILE           output log to FILE\n");
+        printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
+        printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
+        printf("\n");
+        exit(0);
+    }
+  }
+
+  return 0;
+}
+
 
 void init_monitor(int argc, char *argv[]) {
   /* Perform some global initialization. */
@@ -267,8 +266,9 @@ void init_monitor(int argc, char *argv[]) {
   /* Parse arguments. */
   parse_args(argc, argv);
 
+  #ifdef CONFIG_FTRACE
   parse_elf();
- 
+  #endif
   /* Set random seed. */
   init_rand();
 
