b1817113a57954d2dce64503932238d886f3d678
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 08:30:11 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:30:11 up 1 day, 21:53,  3 users,  load average: 0.26, 0.15, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -8,10 +8,10 @@ void __am_timer_init() {
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t current;
   current = 0;
-  putch('a');
+  //putch('a');
   //current = inl((uintptr_t) 0xa0000048);
   uptime->us = current;
-  putch('b');
+  //putch('b');
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -39,6 +39,7 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  printf("aaaaaaaa\n");
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
@@ -48,6 +49,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
+  printf("bbbbbbbbbb\n");
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
