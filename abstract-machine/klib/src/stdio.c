a52ac8eac193119b6f1212b4ae57ebfdeabc7301
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-02 05:54:28 -0700

    >  compile NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:54:28 up 1 day, 19:17,  3 users,  load average: 0.71, 0.68, 0.31

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,7 +6,91 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  char *s;
+  int d;
+
+  va_start(ap,fmt);
+
+  char origin_str[100];
+  int i = 0;
+  while(*(fmt+i) != '\0')
+  {
+    origin_str[i] = *(fmt+i);
+    i++;
+  }
+  origin_str[i] = '\0';
+  i = 0;
+  int num = 0;
+  while(1)
+  {
+    if(origin_str[i] == '\0')
+    {
+      va_end(ap);
+      return num;
+    }
+    if((origin_str[i] != '%') || (origin_str[i] != '\\') )
+    {
+      putch(origin_str[i]);
+      i++;
+      continue;
+    }
+    else
+    {
+      if((origin_str[i] == '\\'))
+      {
+        i++;
+        switch (origin_str[i])
+        {
+        case  'n':
+          putch('\n');
+          break;
+        
+        default:
+          break;
+        }
+      }
+      else
+      {
+        i++;
+      if(origin_str[i] == 'd')
+      {
+        d = va_arg(ap, int);
+        num++;
+        i++;
+        char inverted_num[20];
+        int len = 0;
+        while(d != 0)
+        {
+          int res = d % 10;
+          inverted_num[len] = (char)(res + 48);
+          len++;
+          d = (d - res)/10;
+        }
+        while(len > 0)
+        {
+          putch(inverted_num[len-1]);
+          len--;
+        }
+      }
+      if(origin_str[i] == 's')
+      {
+        s = va_arg(ap, char *);
+        num++;
+        i++;
+        while(*s != '\0')
+        {
+          putch(*s);
+          s++;
+        }
+      }
+      }
+      
+
+    }
+    
+  }
+  
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
