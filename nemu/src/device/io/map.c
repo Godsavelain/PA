ae38152102041eb1eb0000042b4a28a66435eb86
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-03 07:13:19 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-37-generic #42~20.04.1-Ubuntu SMP Tue Mar 15 15:44:28 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:13:19 up 2 days, 11:31,  4 users,  load average: 0.53, 0.56, 0.43

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -44,7 +44,12 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
-  //printf("read addr %d from %s of length%d \n",addr,map->name,len);
+  #ifdef CONFIG_DTRACE
+  if(!strcmp("serial",map->name))
+  {
+  printf("read addr %d from %s of length%d \n",addr,map->name,len);
+  }
+  #endif
   return ret;
 }
 
@@ -55,6 +60,11 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
-  //printf("write data %ld to addr %d from %s \n",data,addr,map->name);
+  #ifdef CONFIG_DTRACE
+  if(!strcmp("serial",map->name))
+  {
+  printf("write data %ld to addr %d from %s \n",data,addr,map->name);
+  }
+  #endif
 
 }
