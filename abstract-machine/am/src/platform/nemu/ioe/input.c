cc8b8845c48a0042926fea20fe1adc1e061e1c6b
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 03:33:43 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:33:43 up  3:03,  3 users,  load average: 0.59, 0.37, 0.26

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -3,13 +3,31 @@
 #include <stdio.h>
 
 #define KEYDOWN_MASK 0x8000
+// const char * const keys[] = {
+//   "ESCAPE","F1","F2","F3","F4","F5","F6","F7","F8", "F9","F10","F11","F12",
+//   "GRAVE", "1","2","3","4","5","6","7","8","9","0","MINUS","EQUALS","BACKSPACE", 
+//   "TB","Q","W","E","R","T","Y","U","I", "O","P","LEFTBRACKET","RIGHTBRACKET","BACKSLASH",
+//   "CPSLOCK","A","S","D","F","G","H","J","K","L","SEMICOLON","APOSTROPHE","RETURN",
+//   "LSHIFT","Z","X","C","V","B","N","M","COMMA","PERIOD","SLASH","RSHIFT", 
+//   "LCTRL","APPLICATION","LALT","SPACE","RLT","RTRL","UP","DOWN","LEFT","RIGHT",
+//   "INSERT","DELETE","HOME","END","PAGEUP","PAGEDOWN"
+// };
+// bool key_pushed[100] = {false};
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t data = *(volatile uint32_t *)(KBD_ADDR);
-  if((data>0)&&(data < 100000)) 
+  if(data > 100)
   {
-    printf("d: %d\n",data);
+    int num = data - 32768;
+    //key_pushed[num] = true;
+    //printf("key %s pushed down\n",keys[num]);
+    kbd->keydown = true;
+    kbd->keycode = num;
   }
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  else
+  {
+    kbd->keydown = false;
+    kbd->keycode = data;
+  }
+
 }
