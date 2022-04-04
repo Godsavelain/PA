a0e34c78a492b6993f785453c2f1b8bda96371db
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-04 00:51:41 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:51:35 up 21 min,  3 users,  load average: 0.89, 1.18, 1.17

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -6,7 +6,10 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t data = *(volatile uint32_t *)(KBD_ADDR);
-  if((data>=0)&&(data < 100000)) printf("d:   %d\n",data);
+  if((data>0)&&(data < 100000)) 
+  {
+    printf("d: %d\n",data);
+  }
   kbd->keydown = 0;
   kbd->keycode = AM_KEY_NONE;
 }
