d61db69ecf017196c0bad074bc0014ec348897ed
tracer-ysyx2204 <tracer@ysyx.org>
2022-04-12 08:56:32 -0700

    >  generate verilog ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:56:32 up 4 days,  4:29,  6 users,  load average: 0.04, 0.19, 0.21

--- npc/playground/src/Elaborate.scala
+++ npc/playground/src/Elaborate.scala
@@ -1,4 +1,4 @@
 import patchouli._
 object Elaborate extends App {
-  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new Decode())))
+  (new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new Mem())))
 }
