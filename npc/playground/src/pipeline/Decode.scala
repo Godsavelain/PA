61177dfaa2fe52ed9d11b1587d1604fdb5621bd3
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-01 08:05:40 -0700

    >  generate verilog ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:05:40 up  6:32,  3 users,  load average: 0.08, 0.11, 0.08

--- npc/playground/src/pipeline/Decode.scala
+++ npc/playground/src/pipeline/Decode.scala
@@ -36,7 +36,7 @@ class Decode extends Module{
 
   val ctrl = ListLookup(inst,
     //   v  fu_code alu_code  jmp_code  mem_code mem_size   csr_code  mdu_code  w  rs1_src       rs2_src  rd_en  imm_type
-    List(N, FU_X,   ALU_X,    JMP_X,    MEM_X,   MEM_X,     CSR_X,     N, RS_X,         RS_X,        N, IMM_X    ,MDU_X),
+    List(N, FU_X,   ALU_X,    JMP_X,    MEM_X,   MEM_X,     CSR_X,  MDU_X,   N, RS_X,         RS_X,        N, IMM_X  ),
     Array(
       // RV32I
       LUI     ->  List(Y, FU_ALU, ALU_ADD,  JMP_X,    MEM_X,   MEM_X,     CSR_X,   MDU_X,    N, RS_FROM_ZERO, RS_FROM_IMM, Y, IMM_U   ),
