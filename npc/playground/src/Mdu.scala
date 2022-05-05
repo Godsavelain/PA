94413e0613d08661d0033e7608ac5f03b7d2ccc6
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 05:32:24 -0700

    >  generate verilog ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:32:24 up  2:50,  3 users,  load average: 0.18, 0.16, 0.14

--- npc/playground/src/Mdu.scala
+++ npc/playground/src/Mdu.scala
@@ -170,7 +170,8 @@ class Mul extends Module {
   val temp_out = Wire(UInt(128.W))
 
   for (i <- 0 until 11) {
-    csa(i).io.x := Cat(pp(i * 3)(127 - 6 * i, 0).asUInt, Fill(6 * i, "b0".U))
+    val experi = Cat(pp(i * 3)(127 - 6 * i, 0), Fill(6 * i, "b0".U))
+    csa(i).io.x := Cat(pp(i * 3)(127 - 6 * i, 0), Fill(6 * i, "b0".U))
     csa(i).io.y := Cat(pp(i * 3 + 1)(125 - 6 * i, 0), Fill(6 * i + 2, "b0".U))
     csa(i).io.z := Cat(pp(i * 3 + 2)(123 - 6 * i, 0), Fill(6 * i + 4, "b0".U))
     s_l1(i) := csa(i).io.s
