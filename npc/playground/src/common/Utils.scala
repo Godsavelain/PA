8a3c0569bb7a2af0cb59ca0e895834665e054124
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 04:52:53 -0700

    >  generate verilog ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:52:53 up  2:11,  3 users,  load average: 0.51, 0.39, 0.19

---
+++ npc/playground/src/common/Utils.scala
@@ -0,0 +1,11 @@
+package patchouli
+import chisel3._
+import chisel3.util._
+
+object Zext32_64 {
+  def apply(x: UInt): UInt = Cat(Fill(32 , 0.U) , x)
+}
+
+object Sext32_64 {
+  def apply(x: UInt): UInt = Cat(Fill(32, x(31)), x)
+}
