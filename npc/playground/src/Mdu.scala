39cf4fa1fabb0d053cc38781ec355258f6e7c214
tracer-ysyx2204 <tracer@ysyx.org>
2022-05-05 04:43:33 -0700

    >  generate verilog ysyx_22040135 李宗泽 Linux ubuntu 5.13.0-40-generic #45~20.04.1-Ubuntu SMP Mon Apr 4 09:38:31 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:43:33 up  2:01,  3 users,  load average: 0.26, 0.08, 0.03

--- npc/playground/src/Mdu.scala
+++ npc/playground/src/Mdu.scala
@@ -49,8 +49,8 @@ class Csa extends Module{
     io.c := Cat(((io.x & io.y) | (io.y & io.z) | (io.z & io.x))(126,0) , "b0".U)
 }
 
-class Mul extends Module{
-  val io = IO(new Bundle{
+class Mul extends Module {
+  val io = IO(new Bundle {
     val in1 = Input(UInt(65.W))
     val in2 = Input(UInt(65.W))
     val out = Output(UInt(128.W))
@@ -58,7 +58,7 @@ class Mul extends Module{
     val mul_ready = Output(Bool())
   })
 
-  val s_idle :: s_1 :: s_2 :: s_3:: Nil = Enum(4)
+  val s_idle :: s_1 :: s_2 :: s_3 :: Nil = Enum(4)
   val state = RegInit(s_idle)
   val reg_in1 = RegInit(0.U(65.W))
   val reg_in2 = RegInit(0.U(67.W))
@@ -69,18 +69,18 @@ class Mul extends Module{
     val pp = RegInit(0.U(128.W))
     pp
   }
-//level 2
-//  2
-//  3
-//  4
-//  6
+  //level 2
+  //  2
+  //  3
+  //  4
+  //  6
   //cut from here
-//  8
-//  11
-//  16
-//  23
-//  34
-//total 32 csas
+  //  8
+  //  11
+  //  16
+  //  23
+  //  34
+  //total 32 csas
 
   val pp_l2 = for (i <- 0 until 8) yield {
     val pp_l2 = RegInit(0.U(128.W))
@@ -94,8 +94,8 @@ class Mul extends Module{
 
   for (i <- 0 until 33) {
     booth(i).io.x := reg_in1
-    booth(i).io.y := reg_in2(i*2+2 , i*2)
-    c(i*2+1,i*2) := booth(i).io.c
+    booth(i).io.y := reg_in2(i * 2 + 2, i * 2)
+    c(i * 2 + 1, i * 2) := booth(i).io.c
   }
 
   val pp33 = RegInit(0.U(128.W))
@@ -168,137 +168,200 @@ class Mul extends Module{
   val temp_out = Wire(UInt(128.W))
 
   for (i <- 0 until 11) {
-    csa(i).io.x := Cat(pp(i*3)(127-6*i ,0 ) ,Fill(6*i ,0.U) )
-    csa(i).io.y := Cat(pp(i*3 + 1)(125-6*i ,0 ) ,Fill(6*i + 2 ,0.U) )
-    csa(i).io.z := Cat(pp(i*3 + 2)(123-6*i ,0 ) ,Fill(6*i + 4 ,0.U) )
+    csa(i).io.x := Cat(pp(i * 3)(127 - 6 * i, 0), Fill(6 * i, 0.U))
+    csa(i).io.y := Cat(pp(i * 3 + 1)(125 - 6 * i, 0), Fill(6 * i + 2, 0.U))
+    csa(i).io.z := Cat(pp(i * 3 + 2)(123 - 6 * i, 0), Fill(6 * i + 4, 0.U))
     s_l1(i) := csa(i).io.s
     c_l1(i) := csa(i).io.c
   }
   //level 2
-  csa(11).io.x := s_l1(0) ; csa(11).io.y := c_l1(0) ; csa(11).io.z := c_l1(1) ;
-  csa(12).io.x := s_l1(1) ; csa(12).io.y := c_l1(2) ; csa(12).io.z := s_l1(2) ;
-  csa(13).io.x := s_l1(3) ; csa(13).io.y := c_l1(3) ; csa(13).io.z := c_l1(4) ;
-  csa(14).io.x := s_l1(4) ; csa(14).io.y := c_l1(5) ; csa(14).io.z := s_l1(5) ;
-  csa(15).io.x := s_l1(6) ; csa(15).io.y := c_l1(6) ; csa(15).io.z := c_l1(7) ;
-  csa(16).io.x := s_l1(7) ; csa(16).io.y := c_l1(8) ; csa(16).io.z := s_l1(8) ;
-  csa(17).io.x := s_l1(9) ; csa(17).io.y := c_l1(9) ; csa(17).io.z := pp33;
+  csa(11).io.x := s_l1(0);
+  csa(11).io.y := c_l1(0);
+  csa(11).io.z := c_l1(1);
+  csa(12).io.x := s_l1(1);
+  csa(12).io.y := c_l1(2);
+  csa(12).io.z := s_l1(2);
+  csa(13).io.x := s_l1(3);
+  csa(13).io.y := c_l1(3);
+  csa(13).io.z := c_l1(4);
+  csa(14).io.x := s_l1(4);
+  csa(14).io.y := c_l1(5);
+  csa(14).io.z := s_l1(5);
+  csa(15).io.x := s_l1(6);
+  csa(15).io.y := c_l1(6);
+  csa(15).io.z := c_l1(7);
+  csa(16).io.x := s_l1(7);
+  csa(16).io.y := c_l1(8);
+  csa(16).io.z := s_l1(8);
+  csa(17).io.x := s_l1(9);
+  csa(17).io.y := c_l1(9);
+  csa(17).io.z := pp33;
 
-  s_l2(0) := csa(11).io.s ; c_l2(0) := csa(11).io.c
-  s_l2(1) := csa(12).io.s ; c_l2(1) := csa(12).io.c
-  s_l2(2) := csa(13).io.s ; c_l2(2) := csa(13).io.c
-  s_l2(3) := csa(14).io.s ; c_l2(3) := csa(14).io.c
-  s_l2(4) := csa(15).io.s ; c_l2(4) := csa(15).io.c
-  s_l2(5) := csa(16).io.s ; c_l2(5) := csa(16).io.c
-  s_l2(6) := csa(17).io.s ; c_l2(6) := csa(17).io.c
+  s_l2(0) := csa(11).io.s;
+  c_l2(0) := csa(11).io.c
+  s_l2(1) := csa(12).io.s;
+  c_l2(1) := csa(12).io.c
+  s_l2(2) := csa(13).io.s;
+  c_l2(2) := csa(13).io.c
+  s_l2(3) := csa(14).io.s;
+  c_l2(3) := csa(14).io.c
+  s_l2(4) := csa(15).io.s;
+  c_l2(4) := csa(15).io.c
+  s_l2(5) := csa(16).io.s;
+  c_l2(5) := csa(16).io.c
+  s_l2(6) := csa(17).io.s;
+  c_l2(6) := csa(17).io.c
 
   //level 3
-  csa(18).io.x := s_l1(10) ; csa(18).io.y := c_l1(10) ; csa(18).io.z := s_l2(0);
-  csa(19).io.x := c_l2(0) ; csa(19).io.y := s_l2( 1); csa(19).io.z := c_l2(1);
-  csa(20).io.x := s_l2( 2); csa(20).io.y := c_l2( 2); csa(20).io.z := s_l2(3);
-  csa(21).io.x := c_l2( 3); csa(21).io.y := s_l2( 4); csa(21).io.z := c_l2(4);
-  csa(22).io.x := s_l2( 5); csa(22).io.y := c_l2( 5); csa(22).io.z := s_l2(6);
+  csa(18).io.x := s_l1(10);
+  csa(18).io.y := c_l1(10);
+  csa(18).io.z := s_l2(0);
+  csa(19).io.x := c_l2(0);
+  csa(19).io.y := s_l2(1);
+  csa(19).io.z := c_l2(1);
+  csa(20).io.x := s_l2(2);
+  csa(20).io.y := c_l2(2);
+  csa(20).io.z := s_l2(3);
+  csa(21).io.x := c_l2(3);
+  csa(21).io.y := s_l2(4);
+  csa(21).io.z := c_l2(4);
+  csa(22).io.x := s_l2(5);
+  csa(22).io.y := c_l2(5);
+  csa(22).io.z := s_l2(6);
 
-  s_l3(0) := csa(18).s ; c_l3(0) := csa(18).c ;
-  s_l3(1) := csa(19).s ; c_l3(1) := csa(19).c ;
-  s_l3(2) := csa(20).s ; c_l3(2) := csa(20).c ;
-  s_l3(3) := csa(21).s ; c_l3(3) := csa(21).c ;
-  s_l3(4) := csa(22).s ; c_l3(4) := csa(22).c ;
+  s_l3(0) := csa(18).s;
+  c_l3(0) := csa(18).c;
+  s_l3(1) := csa(19).s;
+  c_l3(1) := csa(19).c;
+  s_l3(2) := csa(20).s;
+  c_l3(2) := csa(20).c;
+  s_l3(3) := csa(21).s;
+  c_l3(3) := csa(21).c;
+  s_l3(4) := csa(22).s;
+  c_l3(4) := csa(22).c;
 
   //level 4
-  csa(23).io.x := c_l2(6) ; csa(23).io.y := s_l3(0) ; csa(23).io.z := c_l3(0);
-  csa(24).io.x := s_l3(1) ; csa(24).io.y := c_l3(1) ; csa(24).io.z := s_l3(2);
-  csa(25).io.x := c_l3(2) ; csa(25).io.y := s_l3(3) ; csa(25).io.z := s_l3(3);
+  csa(23).io.x := c_l2(6);
+  csa(23).io.y := s_l3(0);
+  csa(23).io.z := c_l3(0);
+  csa(24).io.x := s_l3(1);
+  csa(24).io.y := c_l3(1);
+  csa(24).io.z := s_l3(2);
+  csa(25).io.x := c_l3(2);
+  csa(25).io.y := s_l3(3);
+  csa(25).io.z := s_l3(3);
 
-  s_l4(0) := csa(23).s ; c_l4(0) := csa(23).c ;
-  s_l4(1) := csa(24).s ; c_l4(1) := csa(24).c ;
-  s_l4(2) := csa(25).s ; c_l4(2) := csa(25).c ;
+  s_l4(0) := csa(23).s;
+  c_l4(0) := csa(23).c;
+  s_l4(1) := csa(24).s;
+  c_l4(1) := csa(24).c;
+  s_l4(2) := csa(25).s;
+  c_l4(2) := csa(25).c;
 
   //register these 8 values in pp_l2
 
   //level 5
-  csa(26).io.x := pp_l2(0) ; csa(26).io.y := pp_l2(1) ; csa(26).io.z := pp_l2(2);
-  csa(27).io.x := pp_l2(3) ; csa(27).io.y := pp_l2(4) ; csa(27).io.z := pp_l2(5);
+  csa(26).io.x := pp_l2(0);
+  csa(26).io.y := pp_l2(1);
+  csa(26).io.z := pp_l2(2);
+  csa(27).io.x := pp_l2(3);
+  csa(27).io.y := pp_l2(4);
+  csa(27).io.z := pp_l2(5);
 
-  s_l5(0) := csa(26).s ; c_l5(0) := csa(26).c ;
-  s_l5(1) := csa(27).s ; c_l5(1) := csa(27).c ;
+  s_l5(0) := csa(26).s;
+  c_l5(0) := csa(26).c;
+  s_l5(1) := csa(27).s;
+  c_l5(1) := csa(27).c;
 
   //level 6
-  csa(28).io.x := pp_l2(6) ; csa(28).io.y := pp_l2(7) ; csa(28).io.z := s_l5(0);
-  csa(29).io.x := s_l5(1)  ; csa(29).io.y := c_l5(0)  ; csa(29).io.z := c_l5(0);
+  csa(28).io.x := pp_l2(6);
+  csa(28).io.y := pp_l2(7);
+  csa(28).io.z := s_l5(0);
+  csa(29).io.x := s_l5(1);
+  csa(29).io.y := c_l5(0);
+  csa(29).io.z := c_l5(0);
 
-  s_l6(0) := csa(28).s ; c_l6(0) := csa(28).c ;
-  s_l6(1) := csa(29).s ; c_l6(1) := csa(29).c ;
+  s_l6(0) := csa(28).s;
+  c_l6(0) := csa(28).c;
+  s_l6(1) := csa(29).s;
+  c_l6(1) := csa(29).c;
 
   //level 7
-  csa(30).io.x := s_l6(0) ; csa(30).io.y := s_l6(1) ; csa(30).io.z := c_l6(0);
+  csa(30).io.x := s_l6(0);
+  csa(30).io.y := s_l6(1);
+  csa(30).io.z := c_l6(0);
 
-  s_l7 := csa(30).s ; c_l7 := csa(30).c ;
+  s_l7 := csa(30).s;
+  c_l7 := csa(30).c;
 
   //level 8
-  csa(31).io.x := s_l7 ; csa(31).io.y := c_l7 ; csa(31).io.z := c_l6(1);
+  csa(31).io.x := s_l7;
+  csa(31).io.y := c_l7;
+  csa(31).io.z := c_l6(1);
 
-  s_l8 := csa(31).s ; c_l8 := csa(31).c ;
+  s_l8 := csa(31).s;
+  c_l8 := csa(31).c;
   temp_out := s_l8 + c_l8
 
   //csas
 
-//  val pp0 = RegInit(0.U(130.W))
-//  val pp1 = RegInit(0.U(130.W))
-//  val pp2 = RegInit(0.U(130.W))
-//  val pp3 = RegInit(0.U(130.W))
-//  val pp4 = RegInit(0.U(130.W))
-//  val pp5 = RegInit(0.U(130.W))
-//  val pp6 = RegInit(0.U(130.W))
-//  val pp7 = RegInit(0.U(130.W))
-//  val pp8 = RegInit(0.U(130.W))
-//  val pp9 = RegInit(0.U(130.W))
-//  val pp10 = RegInit(0.U(130.W))
-//  val pp11 = RegInit(0.U(130.W))
-//  val pp12 = RegInit(0.U(130.W))
-//  val pp13 = RegInit(0.U(130.W))
-//  val pp14 = RegInit(0.U(130.W))
-//  val pp15 = RegInit(0.U(130.W))
-//  val pp16 = RegInit(0.U(130.W))
-//  val pp17 = RegInit(0.U(130.W))
-//  val pp18 = RegInit(0.U(130.W))
-//  val pp19 = RegInit(0.U(130.W))
-//  val pp20 = RegInit(0.U(130.W))
-//  val pp21 = RegInit(0.U(130.W))
-//  val pp22 = RegInit(0.U(130.W))
-//  val pp23 = RegInit(0.U(130.W))
-//  val pp24 = RegInit(0.U(130.W))
-//  val pp25 = RegInit(0.U(130.W))
-//  val pp26 = RegInit(0.U(130.W))
-//  val pp27 = RegInit(0.U(130.W))
-//  val pp28 = RegInit(0.U(130.W))
-//  val pp29 = RegInit(0.U(130.W))
-//  val pp30 = RegInit(0.U(130.W))
-//  val pp31 = RegInit(0.U(130.W))
-//  val pp32 = RegInit(0.U(130.W))
+  //  val pp0 = RegInit(0.U(130.W))
+  //  val pp1 = RegInit(0.U(130.W))
+  //  val pp2 = RegInit(0.U(130.W))
+  //  val pp3 = RegInit(0.U(130.W))
+  //  val pp4 = RegInit(0.U(130.W))
+  //  val pp5 = RegInit(0.U(130.W))
+  //  val pp6 = RegInit(0.U(130.W))
+  //  val pp7 = RegInit(0.U(130.W))
+  //  val pp8 = RegInit(0.U(130.W))
+  //  val pp9 = RegInit(0.U(130.W))
+  //  val pp10 = RegInit(0.U(130.W))
+  //  val pp11 = RegInit(0.U(130.W))
+  //  val pp12 = RegInit(0.U(130.W))
+  //  val pp13 = RegInit(0.U(130.W))
+  //  val pp14 = RegInit(0.U(130.W))
+  //  val pp15 = RegInit(0.U(130.W))
+  //  val pp16 = RegInit(0.U(130.W))
+  //  val pp17 = RegInit(0.U(130.W))
+  //  val pp18 = RegInit(0.U(130.W))
+  //  val pp19 = RegInit(0.U(130.W))
+  //  val pp20 = RegInit(0.U(130.W))
+  //  val pp21 = RegInit(0.U(130.W))
+  //  val pp22 = RegInit(0.U(130.W))
+  //  val pp23 = RegInit(0.U(130.W))
+  //  val pp24 = RegInit(0.U(130.W))
+  //  val pp25 = RegInit(0.U(130.W))
+  //  val pp26 = RegInit(0.U(130.W))
+  //  val pp27 = RegInit(0.U(130.W))
+  //  val pp28 = RegInit(0.U(130.W))
+  //  val pp29 = RegInit(0.U(130.W))
+  //  val pp30 = RegInit(0.U(130.W))
+  //  val pp31 = RegInit(0.U(130.W))
+  //  val pp32 = RegInit(0.U(130.W))
 
 
   io.mul_ready := (state === s_idle)
   io.out := reg_out
 
-  switch(state){
-    is(s_idle){
-      when(io.mul_valid){
+  switch(state) {
+    is(s_idle) {
+      when(io.mul_valid) {
         state := s_1
         reg_in1 := io.in1
-        reg_in2 := Cat(io.in2(64) ,io.in2, "b0".U)
+        reg_in2 := Cat(io.in2(64), io.in2, "b0".U)
       }
     }
-    is(s_1){
-      when(true.B){
+    is(s_1) {
+      when(true.B) {
         state := s_2
-        pp33 := Cat(Fill(62,0.U),c)
+        pp33 := Cat(Fill(62, 0.U), c)
         for (i <- 0 until 33) {
           pp(i) := booth(i).io.z
         }
       }
     }
-    is(s_2){
-      when(true.B){
+    is(s_2) {
+      when(true.B) {
         state := s_2
         pp_l2(0) := s_l4(0)
         pp_l2(1) := s_l4(1)
@@ -310,13 +373,14 @@ class Mul extends Module{
         pp_l2(7) := c_l3(4)
       }
     }
-    is(s_3){
-      when(true.B){
-      state := s_idle
-      reg_out := temp_out
+    is(s_3) {
+      when(true.B) {
+        state := s_idle
+        reg_out := temp_out
+      }
     }
   }
-
+  
 }
 
 class Div extends Module{
