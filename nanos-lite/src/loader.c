0f29b2c6ec7bdc948a4607b3d50d8784388d2959
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 04:31:44 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:31:44 up  8:27,  4 users,  load average: 0.15, 0.21, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,6 +18,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   uintptr_t entry = 0x83000000;
   Elf_Ehdr *elf_header;
   ramdisk_read(&elf_header, 0, sizeof(Elf64_Ehdr));
+  printf("%p /n",*(uint32_t *)elf_header->e_ident);
   assert(*(uint32_t *)elf_header->e_ident == 0x7f454c4602010100);
   Elf64_Phdr *phdr = (Elf64_Phdr*)malloc(sizeof(Elf64_Phdr) * elf_header->e_phnum);
   ramdisk_read(phdr, elf_header->e_phoff, sizeof(Elf64_Phdr) * elf_header->e_phnum);
