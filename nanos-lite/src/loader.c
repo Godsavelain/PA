99245ce6187d5d3ecc0a52cc54acac5697b45a6b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 04:33:52 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:33:52 up  8:29,  4 users,  load average: 0.67, 0.33, 0.22

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,8 +18,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   uintptr_t entry = 0x83000000;
   Elf_Ehdr *elf_header;
   ramdisk_read(&elf_header, 0, sizeof(Elf64_Ehdr));
-  printf("%p /n",*(uint32_t *)elf_header->e_ident);
-  //assert(*(uint32_t *)elf_header->e_ident == 0x7f454c4602010100);
+  //printf("%p /n",*(uint32_t *)elf_header->e_ident);
+  unsigned int magic_number;
+  magic_number = *(uint32_t *)elf_header->e_ident;
+  // assert(*(uint32_t *)elf_header->e_ident == 0x7f454c4602010100);
+  assert(magic_number == 0x7f454c4602010100);
   Elf64_Phdr *phdr = (Elf64_Phdr*)malloc(sizeof(Elf64_Phdr) * elf_header->e_phnum);
   ramdisk_read(phdr, elf_header->e_phoff, sizeof(Elf64_Phdr) * elf_header->e_phnum);
   int phdr_num = 0;
