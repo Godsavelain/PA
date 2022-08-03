fa9eeaa41dc2dd4738a9d7126d641efd129c1d8e
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 03:07:49 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:07:49 up  7:03,  3 users,  load average: 0.00, 0.06, 0.07

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,36 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  // size_t len = get_ramdisk_size();
+  // char *elf_file = malloc(len * sizeof(char) );
+  // ramdisk_read(elf_file, 0, len);
+  uintptr_t entry = 0x83000000;
+  Elf_Ehdr *elf_header;
+  ramdisk_read(&elf_header, 0, sizeof(Elf64_Ehdr));
+  assert(*(uint32_t *)elf_header->e_ident == 0x7f454c4602010100);
+  Elf64_Phdr *phdr = (Elf64_Phdr*)malloc(sizeof(Elf64_Phdr) * elf_header->e_phnum);
+  ramdisk_read(phdr, elf_header->e_phoff, sizeof(Elf64_Phdr) * elf_header->e_phnum);
+  int phdr_num = 0;
+  Elf64_Off  offset = 0;
+  Elf64_Addr vaddr = 0;
+  Elf64_Xword file_size = 0;
+  Elf64_Xword mem_size = 0;
+  for(;phdr_num < elf_header->e_phnum;phdr_num++)
+  {
+    if(phdr[phdr_num].p_type == PT_LOAD)
+    {
+    offset = phdr[phdr_num].p_offset;
+    vaddr  = phdr[phdr_num].p_vaddr;
+    file_size = phdr[phdr_num].p_filesz;
+    mem_size  = phdr[phdr_num].p_memsz;
+    ramdisk_read((char *)vaddr, offset, file_size);
+    memset((char *)(vaddr + file_size), 0, (mem_size - mem_size));
+    }
+  }
+  return entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
