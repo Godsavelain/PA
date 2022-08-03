33463d28c3f5a2bb7b443c91728c4cdfc0177b42
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-03 04:08:56 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:08:56 up  8:04,  3 users,  load average: 0.47, 0.34, 0.15

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,218 +10,132 @@ bool is_num(char in)
   return ((in >= '0') && (in <= '9'));
 }
 
-int printf(const char *fmt, ...) {
-  va_list ap;
-  char *s;
-  int d;
-  unsigned int u;
-  long long int l;
-  int std_len = 0;
-  bool has_std_width = false;
+void printch(const char ch)   //输出字符
+{  
+    putch(ch);  
+}  
+ 
+ 
+void printint(const int dec)     //输出整型数
+{  
+    if(dec == 0)  
+    {  
+        return;  
+    }  
+    printint(dec / 10);  
+    putch((char)(dec % 10 + '0'));  
+}
 
-  va_start(ap,fmt);
-
-  char origin_str[100];
-  int i = 0;
-  while(*(fmt+i) != '\0')
-  {
-    origin_str[i] = *(fmt+i);
-    i++;
-  }
-  origin_str[i] = '\0';
-  i = 0;
-  int num = 0;
-  while(1)
-  {
-    if(origin_str[i] == '\0')
-    {
-      va_end(ap);
-      return num;
-    }
-    if((origin_str[i] != '%') && (origin_str[i] != '\\') )
-    {
-      putch(origin_str[i]);
-      i++;
-      continue;
-    }
+void printhex(const int dec)     //output hex
+{  
+    int num = 0;
+    if(dec == 0)  
+    {  
+        return;  
+    }  
+    printhex(dec / 16); 
+    num = dec % 16;
+    if(num < 10){
+      putch((char)(num + '0'));  
+    } 
     else
     {
-      if((origin_str[i] == '\\'))
-      {
-        i++;
-        switch (origin_str[i])
-        {
-        case  'n':
-          putch('\n');
-          break;
-        
-        default:
-          break;
-        }
-      }
-      else
-      {
-        i++;
-      if(is_num(origin_str[i]))
-      {
-        std_len = origin_str[i] - '0';
-        has_std_width = true;
-        i++;
-        while(is_num(origin_str[i]))
-        {
-          std_len = std_len * 10 + (origin_str[i] - '0');
-          i++;
-        }
-      }
-      if((origin_str[i] == 'l') && (origin_str[i+1] == 'd'))
-      {
-        l = va_arg(ap, long long int);
-        num++;
-        i = i+2;
-        char inverted_num[20];
-        int len = 0;
-        if(l == 0)
-        {
-          if(has_std_width)
-        {
-          for(int i=0;i<(std_len-1);i++)
-          {
-            putch('0');
-          }
-        }
-          putch('0');
-        }
-        else
-        {
-          while(l != 0)
-        {
-          int res = l % 10;
-          inverted_num[len] = (char)(res + 48);
-          len++;
-          l = (l - res)/10;
-        }
-        if(has_std_width)
-        {
-          for(int i=0;i<(std_len - len);i++)
-          {
-            putch('0');
-          }
-        }
-        while(len > 0)
-        {
-          putch(inverted_num[len-1]);
-          len--;
-        }
-        }
-        has_std_width = false;
-        std_len = 0;
-      }
-      if(origin_str[i] == 'd')
-      {
-        d = va_arg(ap, int);
-        num++;
-        i++;
-        char inverted_num[20];
-        int len = 0;
-        if(d == 0)
-        {
-          if(has_std_width)
-        {
-          for(int i=0;i<(std_len-1);i++)
-          {
-            putch('0');
-          }
-        }
-        putch('0');
-        }
-        else
-        {
-          while(d != 0)
-        {
-          int res = d % 10;
-          inverted_num[len] = (char)(res + 48);
-          len++;
-          d = (d - res)/10;
-        }
-        if(has_std_width)
-        {
-          for(int i=0;i<(std_len - len);i++)
-          {
-            putch('0');
-          }
-        }
-        while(len > 0)
-        {
-          putch(inverted_num[len-1]);
-          len--;
-        }
-        }
-        has_std_width = false;
-        std_len = 0;
-      }
-      if(origin_str[i] == 'u')
-      {
-        u = va_arg(ap, unsigned int);
-        num++;
-        i++;
-        char inverted_num[20];
-        int len = 0;
-        if(u == 0)
-        {
-          if(has_std_width)
-        {
-          for(int i=0;i<(std_len-1);i++)
-          {
-            putch('0');
-          }
-        }
-        putch('0');
-        }
-        else
-        {
-          while(u != 0)
-        {
-          unsigned int res = u % 10;
-          inverted_num[len] = (char)(res + 48);
-          len++;
-          u = (u - res)/10;
-        }
-        if(has_std_width)
-        {
-          for(int i=0;i<(std_len - len);i++)
-          {
-            putch('0');
-          }
-        }
-        while(len > 0)
-        {
-          putch(inverted_num[len-1]);
-          len--;
-        }
-        }
-        has_std_width = false;
-        std_len = 0;
-      }
-      if(origin_str[i] == 's')
-      {
-        s = va_arg(ap, char *);
-        num++;
-        i++;
-        while(*s != '\0')
-        {
-          putch(*s);
-          s++;
-        }
-      }
-
-      }
-      
-
+      putch((char)((num - 10) + 'A'));  
     }
-    
+} 
+ 
+ 
+void printstr(const char *ptr)        //输出字符串
+{  
+    while(*ptr)  
+    {  
+        putch(*ptr);  
+        ptr++;  
+    }  
+}  
+ 
+ 
+void printfloat(const float flt)     //输出浮点数，小数点第5位四舍五入
+{  
+    int tmpint = (int)flt;  
+    int tmpflt = (int)(100000 * (flt - tmpint));  
+    if(tmpflt % 10 >= 5)  
+    {  
+        tmpflt = tmpflt / 10 + 1;  
+    }  
+    else  
+    {  
+        tmpflt = tmpflt / 10;  
+    }  
+    printint(tmpint);  
+    putch('.');  
+    printint(tmpflt);  
+ 
+}  
+ 
+
+int printf(const char *fmt, ...) {
+  va_list ap;  
+  va_start(ap,fmt);     //将ap指向第一个实际参数的地址
+    while(*fmt)  
+    {  
+        if(*fmt != '%')  
+        {  
+            putch(*fmt);  
+            fmt++;  
+        }  
+        else  
+        {  
+            fmt++;  
+            switch(*fmt)  
+            {  
+                case 'c':  
+                {  
+                    char valch = va_arg(ap,int);  //记录当前实践参数所在地址
+                    printch(valch);  
+                    fmt++;  
+                    break;  
+                }  
+                case 'd':  
+                {  
+                    int valint = va_arg(ap,int);  
+                    printint(valint);  
+                    fmt++;  
+                    break;  
+                }  
+                case 'p':  
+                {  
+                    int valint = va_arg(ap,int);  
+                    printhex(valint);  
+                    fmt++;  
+                    break;  
+                } 
+                case 's':  
+                {  
+                    char *valstr = va_arg(ap,char *);  
+                    printstr(valstr);  
+                    fmt++;  
+                    break;  
+                }  
+                case 'f':  
+                {  
+                    float valflt = va_arg(ap,double);  
+                    printfloat(valflt);  
+                    fmt++;  
+                    break;  
+                }  
+                default:  
+                {  
+                    printch(*fmt);  
+                    fmt++;  
+                }  
+          }    
+      }  
   }
-  
+    va_end(ap);
+    return 1;
 }
+
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
   panic("Not implemented");
