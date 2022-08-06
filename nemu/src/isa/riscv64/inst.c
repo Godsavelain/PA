a9961628bd9f0af7e4cea2c36a0f5268194b7a53
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 05:15:10 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:15:10 up  8:23,  3 users,  load average: 0.66, 0.34, 0.13

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -230,9 +230,10 @@ int isa_exec_once(Decode *s) {
   //     trace_num--;
   //   }
   // }
-  // if(s->pc > 0x83000000)
-  // {
-  //   printf("pc %lx \n",s->pc);
-  // }
+  if(s->pc > 0x83000000)
+  {
+    printf("pc %lx \n",s->pc);
+    printf("inst: %x \n",s->isa.inst.val);
+  }
   return decode_exec(s);
 }
