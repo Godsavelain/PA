bfce4fea101cb9e91569fd95dcb65b6572591eaf
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-06 06:19:23 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-43-generic #46~20.04.1-Ubuntu SMP Thu Jul 14 15:20:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:19:23 up  9:27,  3 users,  load average: 0.92, 0.48, 0.31

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,16 @@
 #include <common.h>
 
+void do_syscall(Context *c);
+
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-    case 1: printf("EXCEPTION YIELD!\n");break;//YIELD
+    case 1: 
+    printf("EXCEPTION YIELD!\n");
+    do_syscall(c);
+    break;//YIELD
     default: panic("Unhandled event ID = %d", e.event);
   }
-
+  
   return c;
 }
 
