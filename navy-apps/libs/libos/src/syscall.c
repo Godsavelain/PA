131fa2853cba80260c8fce51fcb7f2e66d27b49f
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-10 20:26:05 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:26:05 up 26 min,  4 users,  load average: 1.32, 0.87, 0.52

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -68,7 +68,7 @@ int _open(const char *path, int flags, mode_t mode) {
 int _write(int fd, void *buf, size_t count) {
   int suc_num = 0;
   suc_num = _syscall_(SYS_write, fd , (intptr_t)buf, count);
-
+  assert(suc_num == 1);
   //_exit(SYS_write);
   return suc_num;
 }
