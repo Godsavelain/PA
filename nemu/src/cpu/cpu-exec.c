98bdb748b4db6ab6b9e87a49d98ab92d14ef4260
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 22:53:16 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:53:16 up  3:11,  6 users,  load average: 0.88, 0.63, 0.48

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,9 +80,14 @@ static void exec_once(Decode *s, vaddr_t pc) {
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
   printf("pc %lx true inst %x \n",s->pc,s->isa.inst.val);
   
-  for (i = 0; i < ilen; i ++) {
+  // for (i = 0; i < ilen; i ++) {
+  //   p += snprintf(p, 4, " %02x", inst[i]);
+  // }
+
+for (i = ilen; i > ilen; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
+
   printf("inst %s \n",s->logbuf);
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
   int space_len = ilen_max - ilen;
