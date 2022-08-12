27e7a31a8a7b389bbcf7985a5140b572a36c7dcd
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-12 05:47:45 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:47:45 up 10:06,  8 users,  load average: 0.27, 0.37, 0.23

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -58,7 +58,7 @@ void do_syscall(Context *c) {
 
     case 9://SYS_brk
       printf("syscall brk\n");
-      printf("inc %d\n",(int)c->GPR2);
+      printf("inc %x\n",(int)c->GPR2);
       c->GPRx = 0;
       break;
 
