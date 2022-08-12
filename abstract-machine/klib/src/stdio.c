c374cef484b63d5417bbeb500852c44d6765b4a9
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 22:34:15 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:34:15 up  2:52,  6 users,  load average: 0.66, 0.27, 0.19

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -181,7 +181,7 @@ int sprintf(char *out, const char *fmt, ...)
 
   va_start(ap,fmt);
 
-  char origin_str[100];
+  char origin_str[1000];
   int i = 0;
   while(*(fmt+i) != '\0')
   {
@@ -250,7 +250,158 @@ int sprintf(char *out, const char *fmt, ...)
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  char *s;
+  int d;
+
+  va_start(ap,fmt);
+
+  char origin_str[1000];
+  int i = 0;
+  while(*(fmt+i) != '\0')
+  {
+    origin_str[i] = *(fmt+i);
+    i++;
+  }
+  origin_str[i] = '\0';
+  i = 0;
+  int num = 0;
+  int width = 0;
+  bool fill_zero = false;//%02x zero means fill with zero
+  while(i < n)
+  {
+    if(origin_str[i] == '\0')
+    {
+      va_end(ap);
+      *out = '\0';
+      return num;
+    }
+    if(origin_str[i] != '%') 
+    {
+      *out = origin_str[i];
+      out++;
+      i++;
+      continue;
+    }
+    else
+    {
+      i++;
+      if(origin_str[i] == '0')
+      {
+        fill_zero = true;
+        i++;
+      }
+      else{
+        while((origin_str[i] >= '0') && (origin_str[i] <= '9'))
+        {
+          width = width*10 + (origin_str[i] - '0');
+          i++;
+        }
+      }
+      if(origin_str[i] == 'd')
+      {
+        d = va_arg(ap, int);
+        num++;
+        i++;
+        char inverted_num[20];
+        int len = 0;
+        while(d != 0)
+        {
+          int res = d % 10;
+          inverted_num[len] = (char)(res + 48);
+          len++;
+          d = (d - res)/10;
+        }
+        if(width > 0)
+        {
+          for(int j=0;j < (width-len);j++)
+          {
+            if(fill_zero)
+            {
+              *out = '0';
+              out++;
+            }
+            else
+            {
+              *out = ' ';
+              out++;
+            }
+          }
+          fill_zero = false;
+          width = 0;
+        }
+        while(len > 0)
+        {
+          *out = inverted_num[len-1];
+          out++;
+          len--;
+        }
+      }
+
+    if(origin_str[i] == 'x')
+      {
+        d = va_arg(ap, int);
+        num++;
+        i++;
+        char inverted_num[20];
+        int len = 0;
+        while(d != 0)
+        {
+          int res = d % 16;
+          if(res < 10)
+          {
+            inverted_num[len] = (char)(res + 48);
+          }
+          else
+          {
+            inverted_num[len] = (char)(res + 55);
+          }
+          len++;
+          d = (d - res)/16;
+        }
+        if(width > 0)
+        {
+          for(int j=0;j < (width-len);j++)
+          {
+            if(fill_zero)
+            {
+              *out = '0';
+              out++;
+            }
+            else
+            {
+              *out = ' ';
+              out++;
+            }
+          }
+          fill_zero = false;
+          width = 0;
+        }
+        while(len > 0)
+        {
+          *out = inverted_num[len-1];
+          out++;
+          len--;
+        }
+      }
+
+      if(origin_str[i] == 's')
+      {
+        s = va_arg(ap, char *);
+        num++;
+        i++;
+        while(*s != '\0')
+        {
+          *out = *s;
+          out++;
+          s++;
+        }
+      }
+    } 
+  }
+  *out = '\0';
+  return num;
+
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
