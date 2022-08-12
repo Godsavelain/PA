d456b88d9ec16ca149e24869618e3efb6913cc18
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-11 23:23:05 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:23:05 up  3:41,  6 users,  load average: 0.61, 0.20, 0.19

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -79,6 +79,8 @@ extern "C" void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int
   raw_string_ostream os(s);
   gIP->printInst(&inst, pc, "", *gSTI, os);
  
+  printf("s %s\n",s.c_str());
+
   int skip = s.find_first_not_of('\t');
   const char *p = s.c_str() + skip;
 
