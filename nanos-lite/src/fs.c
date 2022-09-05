b632ed54a2780f23ad4340732c64f24216d1d0ed
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-05 06:21:42 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:21:42 up  2:31,  5 users,  load average: 0.16, 0.32, 0.32

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -60,7 +60,11 @@ size_t fs_read(int fd, void *buf, size_t len){
   }
   ramdisk_read((char*)buf, offset, file_size);
   printf("open_offset %d disk_offset %d offset %d file_size %d\n",file_table[fd].open_offset,file_table[fd].disk_offset,offset,file_size);
-  printf("num:%d \n",((int*)buf)[0]);
+  char *temp;
+  temp = buf;
+  for(int i=0;i<10;i++){
+    printf("i:%d num%c\n",i,temp[i]);
+  }
   return file_size;
 }
 
