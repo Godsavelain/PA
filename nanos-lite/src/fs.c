fbccc9fe278339b638f80bae23234e4180c339b2
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-05 08:47:34 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:47:34 up  4:57,  5 users,  load average: 0.83, 0.53, 0.56

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -61,6 +61,7 @@ size_t fs_read(int fd, void *buf, size_t len){
   int actual_len = len;
   if(len > remain_space){
     actual_len = remain_space;
+    printf("lzzzzzzzz\n");
   }
   // ramdisk_read(buf, offset, actual_len);
   ramdisk_read(buf, offset, actual_len);
