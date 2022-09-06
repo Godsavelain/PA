6537a62dafd767fbc471c278541080988d6b5f00
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-05 20:06:47 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:06:47 up 25 min,  3 users,  load average: 0.19, 0.14, 0.29

--- navy-apps/tests/file-test/main.c
+++ navy-apps/tests/file-test/main.c
@@ -17,9 +17,7 @@ int main() {
     assert(n == i + 1);
   }
 
-  printf("aaa \n");
   fseek(fp, 0, SEEK_SET);
-  printf("aaa \n");
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
