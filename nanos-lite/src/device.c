efa3e59ad28109a7d046a57c33827783470c3ad0
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-10 08:21:54 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:21:54 up 52 min,  3 users,  load average: 0.67, 0.20, 0.13

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -34,28 +34,33 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   size_t read_num = 0;
   char* temp_buf = (char*)buf;
-  for(int i=0;i<len;i++){
-    AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-    if (ev.keycode != AM_KEY_NONE){
-      read_num++;
-      if(ev.keydown){
-      memcpy(temp_buf,"kd ",3);
-      }
-      else{
-        memcpy(temp_buf,"ku ",3);
-      }
-      temp_buf = temp_buf + 3;
-      int key_len = strlen(names[ev.keycode]);
-      memcpy(temp_buf,names[ev.keycode],key_len);
-      temp_buf = temp_buf + key_len;
-      memcpy(temp_buf,"\n",1);
-      temp_buf = temp_buf + 1;
-      printf("%s \n",(char*)buf);
-    }    
+  AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
+  if(ev.keycode == AM_KEY_NONE){
+    return 0;
   }
-  printf("bbb \n");
-  printf("%s \n",(char*)buf);
-  printf("bbb \n");
+  while(ev.keycode != AM_KEY_NONE){
+    int key_len = strlen(names[ev.keycode]);
+    if(read_num + 4 + key_len > len){//exceed the capacity
+      break;
+    }
+    if(ev.keydown){
+    memcpy(temp_buf,"kd ",3);
+    }
+    else{
+      memcpy(temp_buf,"ku ",3);
+    }
+    temp_buf = temp_buf + 3;
+    memcpy(temp_buf,names[ev.keycode],key_len);
+    temp_buf = temp_buf + key_len;
+    memcpy(temp_buf,"\n",1);
+    temp_buf = temp_buf + 1;
+    printf("%s \n",(char*)buf);
+    read_num = read_num + 4 + key_len;
+    ev = io_read(AM_INPUT_KEYBRD);   
+  }
+  // printf("bbb \n");
+  // printf("%s \n",(char*)buf);
+  // printf("bbb \n");
   return read_num;
 }
 
