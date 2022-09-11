31faf6a6d0f9a000a2e9e66447441ee7f7231282
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-11 06:21:02 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  06:21:02 up  1:44,  3 users,  load average: 0.49, 0.74, 0.52

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -79,10 +79,12 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   height = io_read(AM_GPU_CONFIG).height;
   int true_len = 0;
   true_len = sprintf((char*)str,"WIDTH:%d\nHEIGHT:%d\n",width,height) + 1;
+  printf("true len %d len %ld\n",true_len,len);
   if(true_len > len){
     return 0;
   }
   sprintf((char*)buf,"WIDTH:%d\nHEIGHT:%d\n",width,height);
+  printf("buf::%s\n",(char*)buf);
   return true_len;
 }
 
