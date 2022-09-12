41536750cdae2c8044ab251deec70b0792cf5d4b
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 07:23:54 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:23:54 up  1:11,  3 users,  load average: 0.20, 0.39, 0.36

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -101,9 +101,10 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   width = io_read(AM_GPU_CONFIG).width;
   height = io_read(AM_GPU_CONFIG).height;
   int x,y;
-  x = offset % (width * 4);
+  x = ((offset / 4) % width);
   y = offset / (width * 4);
   int x_left = width - x;
+  printf("x_left:%d  num:%d\n",x_left,num);
   while(x_left < num){
     io_write(AM_GPU_FBDRAW, x * width, y * height, (char *)buf, x_left, 1, false);
     printf("fbdraw x:%d y:%d x_left:%d\n",x * width, y * height, x_left);
