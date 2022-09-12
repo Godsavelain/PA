8ed479d4c0497eed946b00430260f89d3bb628b7
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 08:23:01 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  08:23:01 up  2:11,  3 users,  load average: 0.16, 0.15, 0.15

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -107,7 +107,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int x_left = width - x;
   //printf("x_left:%d  num:%d\n",x_left,num);
   while(x_left < (num/4)){
-    io_write(AM_GPU_FBDRAW, x * width, y * height, (char *)buf, x_left, 1, false);
+    io_write(AM_GPU_FBDRAW, x , y , (char *)buf, x_left, 1, false);
     printf("fbdraw x:%d y:%d x_left:%d\n",x * width, y * height, x_left);
     buf = (char*)buf + x_left;
     num = num - x_left;
@@ -119,8 +119,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
       return write_num;
     }
   }
-  printf("offset:%d fbdraw x:%d y:%d len:%d\n",(int)offset ,x, y , (int)len);
-  io_write(AM_GPU_FBDRAW, x * width, y * height, (char *)buf, len, 1, true);
+  printf("offset:%d fbdraw x:%d y:%d len:%d\n",(int)offset ,x, y ,(int)len);
+  io_write(AM_GPU_FBDRAW, x , y , (char *)buf, len, 1, true);
   printf("next\n");
   
   return len;
