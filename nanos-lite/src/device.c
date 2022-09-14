ef63bd03d6d086eb305a221f00d9f4723a4a434b
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-14 10:01:55 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:01:55 up  1:25,  5 users,  load average: 0.71, 0.49, 0.37

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -123,7 +123,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
       return write_num;
     }
   }
-  printf("offset:%d x:%d y:%d len:%d\n",(int)offset ,x, y ,(int)len);
+  //printf("offset:%d x:%d y:%d len:%d\n",(int)offset ,x, y ,(int)len);
   io_write(AM_GPU_FBDRAW, x , y , (char *)buf, len/4, 1, true);
   
   return len;
