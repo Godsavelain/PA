164187423929fd0eeb8989a1dec4c52602da6171
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-15 03:21:50 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:21:50 up  8:46,  4 users,  load average: 0.35, 0.41, 0.46

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -43,7 +43,7 @@ int SDL_WaitEvent(SDL_Event *event) {
     else{
       event->type = SDL_KEYUP;
     }
-    printf("keycode 74:%s key:%s",keyname[74],key);
+    printf("keycode 74:%s key:%s len74:%d len:%d",keyname[74],key,strlen(keyname[74]),strlen(key));
     printf("got key %s keycode %d \n",key,key_code);
     event->key.keysym.sym = key_code;
     return 1;
