50f43bbb368ff3d4dbfc6a1bf8db0a57cba44b94
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-17 22:51:22 -0700

    >  run NEMU ysyx_22040135 李宗泽 Linux ubuntu 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:51:22 up  3:17,  3 users,  load average: 0.46, 0.14, 0.11

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -71,9 +71,9 @@ size_t fs_read(int fd, void *buf, size_t len){
   }
   else{
     if(file_table[fd].open_offset >= file_table[fd].size){
-      printf("off %ld size %ld  len:%ld\n",file_table[fd].open_offset,file_table[fd].size,len);
+      return 0;
     }
-    assert(file_table[fd].open_offset < file_table[fd].size);
+    //assert(file_table[fd].open_offset < file_table[fd].size);
     int remain_space = file_table[fd].size - file_table[fd].open_offset;
     if(len > remain_space){
     actual_len = remain_space ;
